6546aec77a754261db4e2f849aa25f769a3306d6
CelCha <celina_chang@hotmail.com>
2016-09-27 22:26:07 -0700

    Works section update
    -mockups
    -advertisements

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,22 +1,39 @@
 $(document).ready(function(){
 	
 	$('#wemesh_tablet').css("opacity","0");
-	$('#zynga_hackathon, #spartans_speak, #wemesh_press_kit').hide();
-	//Works Navigation
+	$('#zynga_hackathon, #spartans_speak, #wemesh_press_kit, #wemesh_adverts, #wemesh_mockups').hide();
+
+
+
 	$("#wemeshtablet").click(function(){
 		$('html, body').animate({
 			scrollTop: $(".wemeshtablet").offset().top
 		}, 1300);
 	});
 
-	$("#wemeshtablet").mouseover(function(){
-		$('#zynga_hackathon, #spartans_speak, #wemesh_press_kit').hide();
-		$('#wemesh_tablet').fadeTo(200,1);
+		$("#wemeshtablet").mouseover(function(){
+			$('#zynga_hackathon, #spartans_speak, #wemesh_press_kit, #wemesh_adverts, #wemesh_mockups').hide();
+			$('#wemesh_tablet').fadeTo(200,1);
+		});
+		
+		$("#wemeshtablet").mouseout(function(){
+			$('#wemesh_tablet').fadeTo(200,0);
+		});
+
+	$("#wemeshmockups").click(function(){
+		$('html, body').animate({
+			scrollTop: $(".wemeshmockups").offset().top
+		}, 1300);
 	});
-	
-	$("#wemeshtablet").mouseout(function(){
-		$('#wemesh_tablet').fadeTo(200,0);
-	});
+
+		$("#wemeshmockups").mouseover(function(){
+			$('#zynga_hackathon, #spartans_speak,#wemesh_tablet, #wemesh_adverts, #wemesh_press_kit').hide();
+			$('#wemesh_mockups').fadeTo(200,1);
+		});
+		
+		$("#wemeshmockups").mouseout(function(){
+			$('#wemesh_mockups').fadeTo(200,0);
+		});
 
 	$("#wemesh").click(function(){
 		$('html, body').animate({
@@ -24,15 +41,29 @@ $(document).ready(function(){
 		}, 1300);
 	});
 
-	// Description
-	$("#wemesh").mouseover(function(){
-		$('#zynga_hackathon, #spartans_speak,#wemesh_tablet').hide();
-		$('#wemesh_press_kit').fadeTo(200,1);
+		$("#wemesh").mouseover(function(){
+			$('#zynga_hackathon, #spartans_speak,#wemesh_tablet, #wemesh_adverts, #wemesh_mockups').hide();
+			$('#wemesh_press_kit').fadeTo(200,1);
+		});
+		
+		$("#wemesh").mouseout(function(){
+			$('#wemesh_press_kit').fadeTo(200,0);
+		});
+
+	$("#wemeshadverts").click(function(){
+		$('html, body').animate({
+			scrollTop: $(".wemeshadverts").offset().top
+		}, 1300);
 	});
-	
-	$("#wemesh").mouseout(function(){
-		$('#wemesh_press_kit').fadeTo(200,0);
-	});
+
+		$("#wemeshadverts").mouseover(function(){
+			$('#zynga_hackathon, #spartans_speak, #wemesh_press_kit, #wemesh_tablet, #wemesh_mockups').hide();
+			$('#wemesh_adverts').fadeTo(200,1);
+		});
+		
+		$("#wemeshadverts").mouseout(function(){
+			$('#wemesh_adverts').fadeTo(200,0);
+		});
 
 	$("#odin").click(function(){
 		$('html, body').animate({
@@ -40,14 +71,14 @@ $(document).ready(function(){
 		}, 1300);
 	});
 
-	$("#odin").mouseover(function(){
-		$('#wemesh_press_kit, #spartans_speak').hide();
-		$('#zynga_hackathon').fadeTo(200,1);
-	});
-	
-	$("#odin").mouseout(function(){
-		$('#zynga_hackathon').fadeTo(200,0);
-	});
+		$("#odin").mouseover(function(){
+			$('#wemesh_press_kit, #spartans_speak, #wemesh_adverts, #wemesh_tablet, #wemesh_mockups').hide();
+			$('#zynga_hackathon').fadeTo(200,1);
+		});
+		
+		$("#odin").mouseout(function(){
+			$('#zynga_hackathon').fadeTo(200,0);
+		});
 
 
 	$("#spartans").click(function(){
@@ -56,14 +87,14 @@ $(document).ready(function(){
 		}, 1300);
 	});
 
-	$("#spartans").mouseover(function(){
-		$('#zynga_hackathon, #wemesh_press_kit, #wemesh_tablet').hide();
-		$('#spartans_speak').fadeTo(200,1);
-	});
-	
-	$("#spartans").mouseout(function(){
-		$('#spartans_speak').fadeTo(200,0);
-	});
+		$("#spartans").mouseover(function(){
+			$('#zynga_hackathon, #wemesh_press_kit, #wemesh_tablet, #wemesh_adverts, #wemesh_mockups').hide();
+			$('#spartans_speak').fadeTo(200,1);
+		});
+		
+		$("#spartans").mouseout(function(){
+			$('#spartans_speak').fadeTo(200,0);
+		});
 
 
 });
